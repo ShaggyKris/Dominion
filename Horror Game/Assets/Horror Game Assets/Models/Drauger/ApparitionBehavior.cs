93dfdbde24492ef403b66b92b50e454663b5a7da
ShaggyKris <pervs.kris@gmail.com>
2015-11-22 14:46:12 -0400

    Walking animation and locomotion
    Added some fire effects, walking animation, and walking locomotion to
    Apparition

--- Horror Game/Assets/Horror Game Assets/Models/Drauger/ApparitionBehavior.cs
+++ Horror Game/Assets/Horror Game Assets/Models/Drauger/ApparitionBehavior.cs
@@ -7,9 +7,12 @@ public class ApparitionBehavior : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+    
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        Vector3 pos = transform.position;
+        pos.x = pos.x + 0.005f;
+        transform.position = pos;
+    }
 }
