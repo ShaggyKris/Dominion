0a510ea203edab3cc4ac126128599698a62f07cb
ShaggyKris <kristoferpervin@android-6305c0c7038e223d.gateway.2wire.net>
2015-11-22 19:30:38 -0400

    Thing

---
+++ Horror Game/Assets/EnableWalk.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableWalk : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		FindObjectOfType<ApparitionBehavior> ().EN = true;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
--- Horror Game/Assets/Horror Game Assets/Models/Drauger/ApparitionBehavior.cs
+++ Horror Game/Assets/Horror Game Assets/Models/Drauger/ApparitionBehavior.cs
@@ -1,29 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ApparitionBehavior : MonoBehaviour {
-    
-	
-    
-	// Update is called once per frame
-	void Update () {
-
-<<<<<<< Updated upstream
-        //Increases object's x position to travel down hallway
-        Vector3 pos = transform.position;
-        pos.x = pos.x + 0.005f;
-        transform.position = pos;
-       
-        if (transform.position.x > -25) //test to see if you can destory game object at certain position
-        {
-            Destroy(gameObject);
-        }
-    }
-=======
-		Vector3 pos = transform.position;
-		pos.x = pos.x + 0.005f;
-		transform.position = pos;
-
+public class ApparitionBehavior : MonoBehaviour
+{
+	public Animator an;
+	// Use this for initialization
+	void Start ()
+	{
+		an.enabled = false;
 	}
->>>>>>> Stashed changes
+	
+	public bool EN = false;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (EN) {
+			an.enabled = true;
+			//Increases object's x position to travel down hallway
+			Vector3 pos = transform.position;
+			pos.x = pos.x + 0.01f;
+			transform.position = pos;
+       
+			if (transform.position.x > -25) { //test to see if you can destory game object at certain position
+				Destroy (gameObject);
+			}
+		}
+	}
+
 }
